5876819aa2cd8811c1a158b13dc557b5fe58dee1
taylor hartman <sussytech@gmail.com>
2023-04-28 15:43:05 -0400

    display workflow

--- main/main.ino
+++ main/main.ino
@@ -15,15 +15,12 @@ PS2Keyboard keyboard;
 const int rs = 10, en = 16, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-uint8_t salt[32] = { 
-  0x22, 0x3A, 0x6B, 0x5E, 0x4E, 0x01, 0x55, 0x11, 
-  0xA0, 0x63, 0x82, 0x1E, 0x23, 0x9C, 0xE5, 0xF3, 
-  0x5C, 0x17, 0x17, 0x2B, 0x39, 0x84, 0x0D, 0x92, 
-  0xA0, 0xCC, 0xE2, 0xF7, 0xE4, 0x39, 0x21, 0xBC 
-  }; // 256-bit salt
-
 char input[64];
+char account[64];
+
 int len = 0;
+
+int state = 0; //0 for "input account" and 1 for "input password"
 
 void setup() {
   delay(1000);
@@ -34,6 +31,8 @@ void setup() {
   pinMode(SwitchPin, INPUT);
 
   lcd.begin(16,2);
+
+  dispPrompt();
 }
 
 void loop() {
@@ -41,27 +40,45 @@ void loop() {
     // read the next key
     char c = keyboard.read();
     if (digitalRead(SwitchPin) == HIGH) {
+      dispPrompt();
+      state = 2;
       KWrite(c);
     } else {
       // This is the password input mode
       if (c == PS2_ENTER){
-        input[len] = '\0';
-        uint8_t hash[32];
-        calc_sha_256(hash, input, len);
-        for(int i = 0; i < 32; i++){
-          Serial.print(hash[i]);
+        if(state == 0){ //account name being entered
+          input[len] = '\0';
+          strcpy(account, input);
+          clearInput();
+          len = 0;
+          state = 1;
+          dispPrompt();
+        } else if (state == 1){ //password name being entered
+          input[len] = '\0';
+          uint8_t hash[32];
+          char hash_in[128] = {0};
+          // Serial.println(account);
+          // Serial.println(input);
+          strcat(hash_in, account);
+          strcat(hash_in, input);
+          calc_sha_256(hash, hash_in, len);
+          for(int i = 0; i < 32; i++){
+            Serial.print(hash[i]);
+          }
+          clearInput();
+          len = 0;
+          Serial.println();
+          state = 0;
+          dispPrompt();
         }
-        len = 0;
-        Serial.println();
-        lcd.clear();
       } else if(c == PS2_DELETE){
         if(len > 0){len--;}
         input[len] = '\0';
-        LCDStars();
+        dispPrompt();
       } else {
         input[len] = c;
         len++;
-        LCDStars();
+        dispPrompt();
       }
     }
   }
@@ -94,15 +111,30 @@ void KWrite(char c) {
   }
 }
 
-void LCDStars(){
-//prints stars on bottom line for each char in input
+void dispPrompt(){
   lcd.clear();
-  String stars = "";
-  for (int x=0; x < len; x++){
-    stars += "*";
+  if (state == 0){
+    lcd.setCursor(0,0); 
+    lcd.print("Account Name:");
+    lcd.setCursor(0,1); 
+    lcd.print(input);
+  } else if (state == 1){
+    String stars = "";
+    for (int x=0; x < len; x++){
+      stars += "*";
+    }
+    lcd.setCursor(0,0); 
+    lcd.print("Password:");
+    lcd.setCursor(0,1); 
+    lcd.print(stars);
+  } else if (state == 2){
+    lcd.setCursor(0,0); 
+    lcd.print("Pass Through");
   }
-  // lcd.setCursor(0,0); 
-  // lcd.print(input);
-  lcd.setCursor(0,1); 
-  lcd.print(stars);
+}
+
+void clearInput(){
+  for(int x = 0; x < sizeof(input) / sizeof(input[0]); x++){
+    input[x] = '\0';
+  }
 }
