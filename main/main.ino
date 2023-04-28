b2461e93239b8c11ec2e08d7bbbad9eb95e29896
taylor hartman <sussytech@gmail.com>
2023-04-28 16:31:18 -0400

    alpha numeric hash

--- main/main.ino
+++ main/main.ino
@@ -67,7 +67,9 @@ void loop() {
           strcat(hash_in, input);
           calc_sha_256(hash, hash_in, len);
           for(int i = 0; i < 32; i++){
-            Serial.print(hash[i]);
+            // Serial.print(toAlphaNumeric(hash[i]));
+            delay(20);
+            KWrite(toAlphaNumeric(hash[i]));
           }
           clearInput();
           len = 0;
@@ -152,3 +154,7 @@ void clearInput(){
   }
 }
 
+char toAlphaNumeric(int input){
+  char alphanumerics[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+  return alphanumerics[input % 62];
+}
